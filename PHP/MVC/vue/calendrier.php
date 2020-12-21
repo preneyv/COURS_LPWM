5e781b7c5be550e090a7964b7104bb9c64fa01e7
preneyv <valerepreney@hotmail.fr>
2020-12-21 03:02:16 +0100

    cssCalendar

---
+++ PHP/MVC/vue/calendrier.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="calendar.css">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/fontawesome.css" integrity="sha384-eHoocPgXsiuZh+Yy6+7DsKAerLXyJmu2Hadh4QYyt+8v86geixVYwFqUvMU8X90l" crossorigin="anonymous"/>
+        <script src="https://kit.fontawesome.com/57280e8850.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="_headMainGrid">
+            <div id="_headPanelSetWeek"></div>
+            <div id="_headCalendar">
+                <span  id="_prevBtn" class="_changeYear fas fa-angle-left"></span>
+                <h6>2014</h6>
+                <span id="_nextBtn" class="_changeYear fas fa-angle-right"></span>
+            </div>
+        </div>
+        <div id="mainGrid">
+            <div id="_panelSetWeek"></div>
+            <div id="_calendar"></div>
+        </div>
+    </body>
+</html>
